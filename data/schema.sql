2ce46ecea260a994fd0f1d4f840fa53d00ce4e5b
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-02-19 00:38:44 -0600

    add fits and fit_items tables

--- data/schema.sql
+++ data/schema.sql
@@ -4,3 +4,17 @@ CREATE TABLE IF NOT EXISTS items (
     volume INTEGER,
     price REAL
 );
+
+CREATE TABLE IF NOT EXISTS fits (
+    fit_id INTEGER PRIMARY KEY,
+    name TEXT,
+    quantity INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS fit_items (
+    fit_id INTEGER,
+    type_id INTEGER,
+    quantity INTEGER,
+    FOREIGN KEY(fit_id) REFERENCES fits(fit_id),
+    FOREIGN KEY(type_id) REFERENCES items(type_id)
+);
