e94fa353a80052502e7675e9abc29afebd892394
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-02-19 15:50:20 -0600

    refactor database interaction to reflect naming and schema changes

--- doctrine/database.py
+++ doctrine/database.py
@@ -1,44 +1,98 @@
 import sqlite3
 
 from core import config
-from data.select import get_by_name
 
 
-def create_fit(fit, quantity):
+def create_doctrine(eft_dict: dict, required: int) -> int:
+    """Write the eft_dict to database as a doctrine.
+    Set the required parameter as the quantity required to keep in stock
+
+    :param eft_dict: dict a fit as returned by reserves.utils.parsers.parse_eft()
+    :param required: int the quantity required to keep in stock
+    :return: int the doctrine id of the new doctrine
+    """
+
     conn = sqlite3.connect(config.DATABASE_PATH)
     c = conn.cursor()
-    c.execute("SELECT MAX(fit_id) FROM fits;")
+    c.execute("SELECT MAX(id) FROM doctrine;")
     max_id = c.fetchone()[0]
     if max_id:
-        fit_id = int(max_id) + 1
+        doctrine_id = int(max_id) + 1
     else:
-        fit_id = 1
-    c.execute("INSERT INTO fits VALUES (?,?,?);", (fit_id, fit['name'], quantity))
+        doctrine_id = 1
+    c.execute("INSERT INTO doctrine VALUES (?,?,?);", (doctrine_id, eft_dict['name'], required))
 
-    for name, quantity in fit['items'].items():
-        type_id = get_by_name(name)[0]
-        c.execute("INSERT INTO fit_items VALUES (?,?,?);", (fit_id, type_id, quantity))
+    for name, required in eft_dict['items'].items():
+        c.execute("SELECT id FROM item WHERE name=?;", (name,))
+        item_id = c.fetchone()[0]
+        c.execute("INSERT INTO doctrine_item VALUES (?,?,?);", (doctrine_id, item_id, required))
     conn.commit()
     c.close()
     conn.close()
-    return fit_id
+    return doctrine_id
 
 
-def read_fit(fit_id):
-    """Retrieve data for the fit of the given id.
+def _get_doctrine(doctrine_id: int, c: sqlite3.Cursor) -> dict:
+    """Helper function to retrieve data for the fit of the given doctrine_id.
+    Should not be called directly.
+
     Example output:
 
         {
             'name': Tempest Fleet Issue,
             'required': 100,
             'available: 50,
-            'items': [
-                # (name, required, available, price)
-                ('Damage Control II', 1, 1234, 1543000)
-            ]
+            'items': {
+                'name': name,
+                'required': required,
+                'available': available,
+                'items': items
+            }
         }
 
-    :param fit_id: int the id of the fit
+    :param doctrine_id: int the id of the fit
+    :param c: sqlite3.Cursor cursor object
+    :return: dict Ship fitting in dict format
+    """
+
+    # Get the doctrine name and required quantity
+    c.execute("SELECT name, required FROM doctrine WHERE id=?;", (doctrine_id,))
+    doctrine_name, doctrine_required = c.fetchone()
+
+    # Get doctrine availability
+    c.execute(
+        "SELECT MIN(item.available / doctrine_item.required) FROM doctrine_item, item WHERE doctrine_item.item_id=item.id AND doctrine_id=?;",
+        (doctrine_id,))
+    doctrine_available = c.fetchone()[0]
+
+    # Get all items for this doctrine
+    c.execute(
+        "SELECT item.name, doctrine_item.required, item.available, item.price FROM doctrine, item, doctrine_item WHERE doctrine.id=? AND doctrine_item.doctrine_id=? AND doctrine_item.item_id=item.id;",
+        (doctrine_id, doctrine_id))
+    items = {}
+    for item in c.fetchall():
+        item_name, item_required, item_available, item_price = item
+        items[item_name] = {
+            'required': item_required,
+            'available': item_available,
+            'price': item_price
+        }
+
+    doctrine = {
+        'name': doctrine_name,
+        'required': doctrine_required,
+        'available': doctrine_available,
+        'items': items
+    }
+    return doctrine
+
+
+def get_doctrine(doctrine_id: int) -> dict:
+    """Retrieve the doctrine for the given doctrine_id
+
+    See _get_doctrine() for more info
+
+    :param doctrine_id: the id of the doctrine to retrieve
     :return: dict Ship fitting in dict format
     """
 
@@ -46,37 +100,52 @@ def read_fit(fit_id):
     conn = sqlite3.connect(config.DATABASE_PATH)
     c = conn.cursor()
 
-    # Get the name and required quantity
-    c.execute("SELECT name, quantity FROM fits WHERE fit_id=?;", (fit_id,))
-    name, required = c.fetchone()
-
-    # Get the availability of the fit
-    c.execute("SELECT MIN(items.volume / fit_items.quantity) FROM fit_items, items WHERE fit_items.type_id=items.type_id AND fit_id=?;", (fit_id,))
-    available = c.fetchone()[0]
-
-    # get all fit items: (name, required, available, price)
-    c.execute("SELECT items.name, fit_items.quantity, items.volume, items.price FROM fits, items, fit_items WHERE fits.fit_id=? AND fit_items.fit_id=? AND fit_items.type_id=items.type_id;", (fit_id, fit_id))
-    items = [i for i in c.fetchall()]
+    doctrine = _get_doctrine(doctrine_id, c)
 
     # Close connection
     c.close()
     conn.close()
-
-    fit = {
-        'name': name,
-        'required': required,
-        'available': available,
-        'items': items
-    }
-    return fit
+    return doctrine
 
 
-def list_fits():
+def list_doctrines() -> list[dict]:
+    """Retrieve all doctrines and related stats
+
+    Example output:
+        [
+            {
+                'id': 1,
+                'name': 'Ferox',
+                'required': 50,
+                'available': 104,
+                'percent': 100
+            },
+        ]
+
+    :return: list[dict]
+    """
+
     conn = sqlite3.connect(config.DATABASE_PATH)
     c = conn.cursor()
-    c.execute("SELECT fits.fit_id, fits.name, fits.quantity, MIN(items.volume/fit_items.quantity) FROM fits, fit_items, items WHERE fits.fit_id=fit_items.fit_id AND fit_items.type_id=items.type_id GROUP BY fits.fit_id;")
-    fits = [i for i in c.fetchall()]
+    c.execute("SELECT doctrine.id, doctrine.name, doctrine.required, MIN(item.available/doctrine_item.required) FROM doctrine, doctrine_item, item WHERE doctrine.id=doctrine_item.doctrine_id AND doctrine_item.item_id=item.id GROUP BY doctrine.id;")
+    # labels = 'id', 'name', 'required', 'available'
+    # doctrines = [dict(zip(labels, i)) for i in c.fetchall()]
+    doctrines = []
+    for doctrine in c.fetchall():
+        id, name, required, available = doctrine
+        percent = min(int(available / required * 100), 100)
+        d = {
+            'id': id,
+            'name': name,
+            'required': required,
+            'available': available,
+            'percent': percent,
+        }
+        doctrines.append(d)
+
+    doctrines = sorted(doctrines, key=lambda d: d['percent'])
+
     conn.commit()
     c.close()
     conn.close()
-    return fits
+    return doctrines
--- doctrine/views.py
+++ doctrine/views.py
@@ -10,22 +10,22 @@ bp = Blueprint('doctrine', __name__, template_folder=config.TEMPLATES_PATH)
 
 @bp.route('/')
 def index():
-    fits = database.list_fits()
-    return render_template('doctrine/index.html', fits=fits)
+    doctrines = database.list_doctrines()
+    return render_template('doctrine/index.html', doctrines=doctrines)
 
 
 @bp.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        fit_raw = request.form.get('fit')
-        quantity = request.form.get('quantity')
-        fit = parse_eft(fit_raw)
-        fit_id = database.create_fit(fit, quantity)
-        return redirect(url_for('fits.view', fit_id=fit_id))
+        eft_paste = request.form.get('eft')
+        required = request.form.get('required')
+        eft_dict = parse_eft(eft_paste)
+        id = database.create_doctrine(eft_dict, required)
+        return redirect(url_for('doctrine.doctrine', id=id))
     return render_template('doctrine/add.html')
 
 
-@bp.route('/fit/<int:id>')
-def fit(id):
-    fit_dict = database.read_fit(id)
-    return render_template('doctrine/view.html', fit=fit_dict)
+@bp.route('/doctrine/<int:id>')
+def doctrine(id):
+    doctrine_dict = database.get_doctrine(id)
+    return render_template('doctrine/doctrine.html', doctrine=doctrine_dict)
